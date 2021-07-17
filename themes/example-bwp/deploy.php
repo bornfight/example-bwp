0bb1069d94f7f6888f49295e4e8e2981a5d7808b
josip <jmucak@outlook.com>
2021-07-17 19:57:18 +0200

    updated deploy php

--- themes/example-bwp/deploy.php
+++ themes/example-bwp/deploy.php
@@ -1,71 +1,56 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/common.php';
 
 // Project name
-set('application', 'example-bwp');
+set( 'application', 'example-bwp' );
 
-set('release_name', function () {
-    return (string) run('date +"%s"');
-});
+set( 'release_name', function () {
+	return (string) run( 'date +"%s"' );
+} );
 
-set('projects', 'bwp_projects');
-set('projects-staging', 'bwp_projects-staging');
+set( 'projects', 'bwp_projects' );
+set( 'projects-staging', 'bwp_projects-staging' );
 
 // Project repository
-set('repository', 'git@github.com:bornfight/example-bwp.git');
+set( 'repository', 'git@github.com:bornfight/example-bwp.git' );
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
+set( 'git_tty', true );
 
 // Shared files/dirs between deploys
-add('shared_files', []);
-add('shared_dirs', []);
+add( 'shared_files', [] );
+add( 'shared_dirs', [] );
 
 // Writable dirs by web server
-add('writable_dirs', []);
-set('allow_anonymous_stats', false);
+add( 'writable_dirs', [] );
+set( 'allow_anonymous_stats', false );
+
+set( 'keep_releases', 3 );
 
 // Hosts
 
-host('staging')
-    ->hostname('3.127.215.91')
-    ->stage('staging')
-    ->set('branch', 'staging')
-    ->user('bitnami')
-    ->port(22)
-    ->identityFile('~/.ssh/id_rsa')
-    ->forwardAgent(true)
-    ->multiplexing(true)
-    ->set('deploy_path', '~/{{projects-staging}}/{{application}}');
+host( 'staging' )->hostname( '18.185.224.204' )->stage( 'staging' )->set( 'branch', 'staging' )->user( 'bitnami' )->port( 22 )->identityFile( '~/.ssh/id_rsa' )->forwardAgent( true )->multiplexing( true )->set( 'deploy_path', '~/{{projects-staging}}/{{application}}' );
 
-host('production')
-    ->hostname('3.127.215.91')
-    ->stage('production')
-    ->set('branch', 'master')
-    ->user('bitnami')
-    ->port(22)
-    ->identityFile('~/.ssh/id_rsa')
-    ->forwardAgent(true)
-    ->multiplexing(true)
-    ->set('deploy_path', '~/{{projects}}/{{application}}');
+host( 'production' )->hostname( '18.185.224.204' )->stage( 'production' )->set( 'branch', 'master' )->user( 'bitnami' )->port( 22 )->identityFile( '~/.ssh/id_rsa' )->forwardAgent( true )->multiplexing( true )->set( 'deploy_path', '~/{{projects}}/{{application}}' );
 
 /**
  * Main task
  */
-task('deploy', [
-    'deploy:info',
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-])->desc('Deploy your project');
+task( 'deploy', [
+	'deploy:info',
+	'deploy:prepare',
+	'deploy:lock',
+	'deploy:release',
+	'deploy:update_code',
+	'deploy:symlink',
+	'deploy:unlock',
+	'cleanup',
+] )->desc( 'Deploy your project' );
 
 
 // [Optional] if deploy fails automatically unlock.
-after('deploy:failed', 'deploy:unlock');
-after('deploy', 'success');
+after( 'deploy:failed', 'deploy:unlock' );
+after( 'deploy', 'success' );
